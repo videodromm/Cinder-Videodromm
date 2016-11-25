0db9016d33f606f6454fc2b64fd27c1b879a51fd
Bruce LANE <bruce@batchass.fr>
2016-11-25 09:02:59 +0100

    New Videodromm template for Tinderbox

---
+++ templates/Videodromm/src/_TBOX_PREFIX_App.cpp
@@ -0,0 +1,208 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+// UserInterface
+#include "CinderImGui.h"
+// Settings
+#include "VDSettings.h"
+// Session
+#include "VDSession.h"
+// Log
+#include "VDLog.h"
+// UI
+#include "VDUI.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+using namespace VideoDromm;
+
+#define IM_ARRAYSIZE(_ARR)			((int)(sizeof(_ARR)/sizeof(*_ARR)))
+
+class _TBOX_PREFIX_App : public App {
+
+public:
+	void setup() override;
+	void mouseMove(MouseEvent event) override;
+	void mouseDown(MouseEvent event) override;
+	void mouseDrag(MouseEvent event) override;
+	void mouseUp(MouseEvent event) override;
+	void keyDown(KeyEvent event) override;
+	void keyUp(KeyEvent event) override;
+	void fileDrop(FileDropEvent event) override;
+	void update() override;
+	void draw() override;
+	void cleanup() override;
+	void setUIVisibility(bool visible);
+private:
+	// Settings
+	VDSettingsRef				mVDSettings;
+	// Session
+	VDSessionRef				mVDSession;
+	// Log
+	VDLogRef					mVDLog;
+	// UI
+	VDUIRef						mVDUI;
+	// handle resizing for imgui
+	void						resizeWindow();
+	bool						mIsResizing;
+	// imgui
+	float						color[4];
+	float						backcolor[4];
+	int							playheadPositions[12];
+	int							speeds[12];
+
+	float						f = 0.0f;
+	char						buf[64];
+	unsigned int				i, j;
+
+	bool						mouseGlobal;
+
+	string						mError;
+	// fbo
+	bool						mIsShutDown;
+	Anim<float>					mRenderWindowTimer;
+	void						positionRenderWindow();
+	bool						mFadeInDelay;
+};
+
+
+void _TBOX_PREFIX_App::setup()
+{
+	// Settings
+	mVDSettings = VDSettings::create();
+	// Session
+	mVDSession = VDSession::create(mVDSettings);
+	//mVDSettings->mCursorVisible = true;
+	setUIVisibility(mVDSettings->mCursorVisible);
+	mVDSession->getWindowsResolution();
+	mVDUI = VDUI::create(mVDSettings, mVDSession);
+
+	mouseGlobal = false;
+	mFadeInDelay = true;
+	// windows
+	mIsShutDown = false;
+	mIsResizing = true;
+	mRenderWindowTimer = 0.0f;
+	timeline().apply(&mRenderWindowTimer, 1.0f, 2.0f).finishFn([&] { positionRenderWindow(); });
+
+}
+void _TBOX_PREFIX_App::positionRenderWindow() {
+	mVDSettings->iResolution.x = mVDSettings->mRenderWidth;
+	mVDSettings->iResolution.y = mVDSettings->mRenderHeight;
+	mVDSettings->mRenderPosXY = ivec2(mVDSettings->mRenderX, mVDSettings->mRenderY);//20141214 was 0
+	setWindowPos(mVDSettings->mRenderX, mVDSettings->mRenderY);
+	setWindowSize(mVDSettings->mRenderWidth, mVDSettings->mRenderHeight);
+}
+void _TBOX_PREFIX_App::setUIVisibility(bool visible)
+{
+	if (visible)
+	{
+		showCursor();
+	}
+	else
+	{
+		hideCursor();
+	}
+}
+void _TBOX_PREFIX_App::fileDrop(FileDropEvent event)
+{
+	mVDSession->fileDrop(event);
+}
+void _TBOX_PREFIX_App::update()
+{
+	mVDSession->setControlValue(30, getAverageFps());
+	mVDSession->update()
+}
+void _TBOX_PREFIX_App::cleanup()
+{
+	if (!mIsShutDown)
+	{
+		mIsShutDown = true;
+		CI_LOG_V("shutdown");
+		ui::disconnectWindow(getWindow());
+		ui::Shutdown();
+		// save settings
+		mVDSettings->save();
+		mVDSession->save();
+		quit();
+	}
+}
+void _TBOX_PREFIX_App::mouseMove(MouseEvent event)
+{
+	if (!mVDSession->handleMouseMove(event)) {
+		// let your application perform its mouseMove handling here
+	}
+}
+void _TBOX_PREFIX_App::mouseDown(MouseEvent event)
+{
+	if (!mVDSession->handleMouseDown(event)) {
+		// let your application perform its mouseDown handling here
+	}
+}
+void _TBOX_PREFIX_App::mouseDrag(MouseEvent event)
+{
+	if (!mVDSession->handleMouseDrag(event)) {
+		// let your application perform its mouseDrag handling here
+	}	
+}
+void _TBOX_PREFIX_App::mouseUp(MouseEvent event)
+{
+	if (!mVDSession->handleMouseUp(event)) {
+		// let your application perform its mouseUp handling here
+	}
+}
+
+void _TBOX_PREFIX_App::keyDown(KeyEvent event)
+{
+	if (!mVDSession->handleKeyDown(event)) {
+		switch (event.getCode()) {
+		case KeyEvent::KEY_ESCAPE:
+			// quit the application
+			quit();
+			break;
+		case KeyEvent::KEY_h:
+			// mouse cursor and ui visibility
+			mVDSettings->mCursorVisible = !mVDSettings->mCursorVisible;
+			setUIVisibility(mVDSettings->mCursorVisible);
+			break;
+		}
+	}
+}
+void _TBOX_PREFIX_App::keyUp(KeyEvent event)
+{
+	if (!mVDSession->handleKeyUp(event)) {
+	}
+}
+void _TBOX_PREFIX_App::resizeWindow()
+{
+	mVDUI->resize();
+	mVDSession->resize();
+}
+
+void _TBOX_PREFIX_App::draw()
+{
+	gl::clear(Color::black());
+	if (mFadeInDelay) {
+		mVDSettings->iAlpha = 0.0f;
+		if (getElapsedFrames() > mVDSession->getFadeInDelay()) {
+			mFadeInDelay = false;
+			timeline().apply(&mVDSettings->iAlpha, 0.0f, 1.0f, 1.5f, EaseInCubic());
+		}
+	}
+
+	//gl::setMatricesWindow(toPixels(getWindowSize()),false);
+	gl::setMatricesWindow(mVDSettings->mRenderWidth, mVDSettings->mRenderHeight, false);
+	gl::draw(mVDSession->getMixTexture(), getWindowBounds());
+	getWindow()->setTitle(mVDSettings->sFps + " fps Videodromm visualizer");
+	// imgui
+	if (!mVDSettings->mCursorVisible) return;
+
+	mVDUI->Run("UI", (int)getAverageFps());
+	if (mVDUI->isReady()) {
+	}	
+}
+
+
+CINDER_APP(_TBOX_PREFIX_App, RendererGl)
