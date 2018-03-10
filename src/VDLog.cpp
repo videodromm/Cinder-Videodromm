3c852b018616550ab3aba3ee977c6d5b476dd8a7
brucelane <bruce@batchass.fr>
2018-03-10 11:31:25 +0100

    sysLogger

--- src/VDLog.cpp
+++ src/VDLog.cpp
@@ -4,9 +4,16 @@ using namespace VideoDromm;
 
 VDLog::VDLog()
 {
-	//fs::path path;
-	//path = getAppPath() / "log";
-	//auto bLog = log::makeLogger<log::LoggerBreakpoint>();
-	//bLog->setTriggerLevel(log::LEVEL_ERROR);
-	//log::makeLogger<log::LoggerFileRotating>(path.string(), "videodromm.%Y.%m.%d.txt", false);
+	auto sysLogger = log::makeLogger<log::LoggerSystem>();
+	sysLogger->setLoggingLevel(log::LEVEL_WARNING);
+#ifdef _DEBUG
+	/* fs::path path;
+	path = getAppPath() / "log";
+	auto bLog = log::makeLogger<log::LoggerBreakpoint>();
+	bLog->setTriggerLevel(log::LEVEL_ERROR); 
+	log::makeLogger<log::LoggerFileRotating>(path.string(), "videodromm.%Y.%m.%d.txt", false);*/
+	log::makeLogger<log::LoggerFileRotating>("/tmp/videodromm", "videodromm.%Y.%m.%d.txt", false);
+#else
+	
+#endif  // _DEBUG
 }
